b4349f78965158a0d3b8a0a807341517af62f9e5
GuilhermeC <guiguimcouto@gmail.com>
2022-06-30 12:32:17 -0300

    Tirando o loop de testes

--- src/main.cpp
+++ src/main.cpp
@@ -20,44 +20,28 @@ int main(int argc, char const *argv[])
 
     cout << "Running " << program_name << " with instance " << input_file_name << " ... " << endl;
 
-    float avg = 0.0;
-    float quality;
-    float best = 0.0;
-    for (int i = 0; i < 10; i++)
+    Graph *graph = new Graph();
+
+    if (input_file_name[0] >= '2' && input_file_name[0] <= '4')
     {
-        Graph *graph = new Graph();
-
-        if (input_file_name[0] >= '2' && input_file_name[0] <= '4')
-        {
-            leHandover(graph, input_file_name);
-        }
-        else if (input_file_name[0] >= 'R' || input_file_name[0] <= 'S')
-        {
-            leRanRealSparse(graph, input_file_name);
-        }
-        else
-        {
-            cout << "Nao foi possivel abrir o arquivo " << input_file_name;
-            return 1;
-        }
-
-        cout << "\nDados sobre o grafo criado" << endl;
-        cout << "Ordem: " << graph->getOrder() << endl;
-        cout << "Nº arestas inseridas: " << graph->getNumberEdges() << endl;
-        cout << "Nº clusters: " << graph->getNumberClusters() << endl;
-
-        graph->GRASP();
-        quality = graph->totalClustersLoads();
-        if(quality > best)
-            best = quality;
-        avg += quality;
-        cout << "Qualidade = " << quality << endl;
-        cout << endl;
-        cout << "-------------------  " << i << "  ---------------------" << endl;
+        leHandover(graph, input_file_name);
+    }
+    else if (input_file_name[0] >= 'R' || input_file_name[0] <= 'S')
+    {
+        leRanRealSparse(graph, input_file_name);
+    }
+    else
+    {
+        cout << "Nao foi possivel abrir o arquivo " << input_file_name;
+        return 1;
     }
 
-    cout << "BEST = " << best << endl;
-    cout << "AVG = " << avg / 10.0 << endl;
-    
+    cout << "\nDados sobre o grafo criado" << endl;
+    cout << "Ordem: " << graph->getOrder() << endl;
+    cout << "Nº arestas inseridas: " << graph->getNumberEdges() << endl;
+    cout << "Nº clusters: " << graph->getNumberClusters() << endl;
+
+    graph->GRASP();
+
     return 0;
 }
